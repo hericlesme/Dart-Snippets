ad4f0b03c8ca4895e805327e4f6d1ce770f4dd1e
Héricles Emanuel <hericles@lsd.ufcg.edu.br>
2019-04-15 13:47:08 -0300

    hello, world

---
+++ variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = 'Dart';
+  var number = 4;
+  dynamic variable = 'dynamic';
+  print(name + ' is Awesome!');
+  print('${number} is Awesome!');
+  print(variable + ' is Awesome!');
+}
